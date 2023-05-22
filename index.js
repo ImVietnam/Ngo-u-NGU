67723bc39682144474c8dce3089306b4d9fe7e17
I_am_Vietnam <91591390+ImVietnam@users.noreply.github.com>
2023-05-22 09:46:12 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let submit = document.getElementById("submit");
+submit.onclick = function(){
+  let btn1 = document.getElementById("btn1").checked;
+  let btn2 = document.getElementById("btn2").checked;
+  let name = document.getElementById("in").value;
+  let out = document.getElementById("out");
+  if(btn1 == true){
+    out = out.innerHTML = `Hế lô ông già ${name} nghèo khổ giữa trời đông cô đơn`
+  }
+  if(btn2 == true){
+    out = out.innerHTML = `Hế lô bà già ${name} nghèo khổ giữa trời đông cô đơn`
+  }
+  if(btn1 == false && btn2 == false){
+    window.alert("Bạn phải chọn 1 trong 2 lựa chọn")
+  }
+}
